f36f08b001e1a1d3618154395109dc4625d18c3e
Mingang Xu <xmgbg18@gmail.com>
2020-01-23 04:00:14 -0800

    new Q4

--- Q4/tests/solution_test.cc
+++ Q4/tests/solution_test.cc
@@ -2,16 +2,16 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-TEST(HelloWorldShould, ReturnHelloWorld) {
+TEST(InfoShould, ReturnInfo) {
   Solution solution;
   std::string actual1 = solution.PrintName();
-  std::string expected1 = "Jerry";
+  std::string expected1 = "Name: Mingang Xu";
   
   std::string actual2 = solution.PrintMajor();
   std::string expected2 = "Major: Electrical Engineering";
     
   std::string actual3 = solution.PrintInterest();
-  std::string expected3 = "Basketball";
+  std::string expected3 = "Interest: C++ Programming";
   EXPECT_EQ(expected1, actual1);
   EXPECT_EQ(expected2, actual2);
   EXPECT_EQ(expected3, actual3);
